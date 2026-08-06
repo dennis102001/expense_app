8f017a89f61095bd42965da21eba7da7010bad48
Dennis Torres <dennisbed1001@gmail.com>
2026-08-06 23:44:29 +0800

    fix(Controller): make app url avaiable for view files

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,6 +6,7 @@ class Controller {
 
     public function view($name, $data = []){
         extract($data);
+        $appUrl = getenv('APP_URL') ?: ($_ENV['APP_URL'] ?? '');
 
         require_once "../app/views/$name.php";
     }
