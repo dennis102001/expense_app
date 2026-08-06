59a0c0c045a82f1c6936ece32879525d58f8d58c
Dennis Torres <dennisbed1001@gmail.com>
2026-08-06 23:09:05 +0800

    fix(MailModel): modify Mail model to use brevo api

--- app/models/MailModel.php
+++ app/models/MailModel.php
@@ -3,53 +3,57 @@
 namespace models;
 
 use core\Model;
-use PHPMailer\PHPMailer\Exception as PHPMailerException;
-use PHPMailer\PHPMailer\PHPMailer;
 
 class MailModel extends Model{
 
     public static function sendResetLink($email, $resetLink){
         try {
-            $mail = self::createMailer();
-            $mail->addAddress($email);
-            $mail->Subject = "Password Reset";
-            $mail->Body = "This email contains link to reset your password from Expense Tracker App. Disregard if you do not know about this. Click to reset password: <a href='$resetLink'>$resetLink</a>";
-            $mail->send();
-
-            return true;
-        } catch (PHPMailerException $e) {
+            return self::sendViaBrevoAPI($email, "Password Reset", "This email contains link to reset your password from Expense Tracker App. Disregard if you do not know about this. Click to reset password: <a href='$resetLink'>$resetLink</a>");
+        } catch (\Exception $e) {
             return false;
         }
-
     }
 
     public static function sendEmailVerification($email, $link){
         try {
-            $mail = self::createMailer();
-            $mail->addAddress($email);
-            $mail->Subject = "Account Activation";
-            $mail->Body = "This email contains link to activate your account from Expense Tracker App. Disregard if you do not know about this. Click to activate your account: <a href='$link'>$link</a>";
-            $mail->send();
-
-            return true;
-        } catch (PHPMailerException $e) {
+            return self::sendViaBrevoAPI($email, "Account Activation", "This email contains link to activate your account from Expense Tracker App. Disregard if you do not know about this. Click to activate your account: <a href='$link'>$link</a>");
+        } catch (\Exception $e) {
             return false;
         }
-
     }
 
-    public static function createMailer(){
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host = getenv('MAIL_HOST') ?: $_ENV['MAIL_HOST'];
-        $mail->SMTPAuth = true;
-        $mail->Username = getenv('MAIL_USERNAME') ?: $_ENV['MAIL_USERNAME'];
-        $mail->Password = getenv('MAIL_PASSWORD') ?: $_ENV['MAIL_PASSWORD'];
-        $mail->SMTPSecure = getenv('MAIL_ENCRYPTION') ?: $_ENV['MAIL_ENCRYPTION'];
-        $mail->Port = (int) getenv('MAIL_PORT') ?: $_ENV['MAIL_PORT'];
-        $mail->setFrom(getenv('MAIL_USERNAME') ?: $_ENV['MAIL_USERNAME'], getenv('MAIL_FROM_NAME') ?: $_ENV['MAIL_FROM_NAME']);
-        $mail->isHTML(true);
+    private static function sendViaBrevoAPI($toEmail, $subject, $htmlBody){
+        $apiKey = getenv('BREVO_API_KEY') ?: $_ENV['BREVO_API_KEY'];
+        $fromEmail = getenv('MAIL_USERNAME') ?: $_ENV['MAIL_USERNAME'];
+        $fromName = getenv('MAIL_FROM_NAME') ?: $_ENV['MAIL_FROM_NAME'];
 
-        return $mail;
+        $postData = [
+            'sender' => [
+                'name' => $fromName,
+                'email' => $fromEmail
+            ],
+            'to' => [
+                ['email' => $toEmail]
+            ],
+            'subject' => $subject,
+            'htmlContent' => $htmlBody
+        ];
+
+        $ch = curl_init('https://api.brevo.com/v3/smtp/email');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'api-key: ' . $apiKey
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode >= 200 && $httpCode < 300;
     }
+
 }
