451d8429ba8f2f2313a591a9fae9fd1907f2d755
Dennis Torres <dennisbed1001@gmail.com>
2026-08-10 00:46:42 +0800

    feat: update app icon

--- app/views/Dashboard.php
+++ app/views/Dashboard.php
@@ -8,6 +8,7 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="<?=  $appUrl ?>/css/app.css">
+<link rel="icon" type="image/svg+xml" href="<?= $appUrl ?>/den-icon.svg">
 
 </head>
 <body class="bg-gray-100 min-h-screen">
--- app/views/Print.php
+++ app/views/Print.php
@@ -20,6 +20,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="<?=  $appUrl ?>/css/app.css">
+    <link rel="icon" type="image/svg+xml" href="<?= $appUrl ?>/den-icon.svg">
 </head>
 
 <body onload="window.print()" class="bg-gray-100 print:bg-white">
