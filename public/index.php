08544c6efaf944ff312ed3f585205324667bd593
Dennis Torres <dennisbed1001@gmail.com>
2026-08-09 23:45:14 +0800

    fix: remove remaining Composer autoloader reference

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 session_start();
 
 require_once '../app/core/Autoloader.php';
-require_once __DIR__ . '/../vendor/autoload.php';
 
 if (file_exists(__DIR__ . '/../.env')) {
     $env = parse_ini_file(__DIR__ . '/../.env');
